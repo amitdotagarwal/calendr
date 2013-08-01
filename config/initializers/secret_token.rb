c7e732ac5d3e497274d9332844c361ee1e7200eb
neel <neelmani.shankar@gmail.com>
2013-08-01 19:43:12 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calendr::Application.config.secret_token = 'fab2c18e97dcc65e1d4c66086a57ac59170978b9876f6820e5d207f5d32e1c6d78e60cbc3c2902d75bbbd115fade2c123edfc0de116f372b803b4255b88a56b6'
